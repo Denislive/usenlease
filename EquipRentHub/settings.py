fb7e3d5369cdcb5725455a11f7a2d7aecc8ecd0c
Dipace <dennygachemi6@gmail.com>
2024-09-28 16:20:36 +0300

    configured static files

--- EquipRentHub/settings.py
+++ EquipRentHub/settings.py
@@ -165,6 +165,9 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
