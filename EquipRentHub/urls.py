5d474442c1d5f1e56427c1dfa08e2261a4b7a693
Dipace <dennygachemi6@gmail.com>
2024-09-28 17:26:26 +0300

    updated urs.py

--- EquipRentHub/urls.py
+++ EquipRentHub/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 urlpatterns = [
@@ -14,6 +16,9 @@ urlpatterns = [
     # path('api/v1/', include('equipment_management.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Serve static files only if DEBUG is False
-if not settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# # Serve static files only if DEBUG is False
+# if not settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
