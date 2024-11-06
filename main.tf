6760c99f612159edf3bffc0980929c7e2e406ad3
ngumonelson123 <nelsonmbui88@gmail.com>
2024-11-07 01:46:27 +0300

    update main file

--- main.tf
+++ main.tf
@@ -34,10 +34,16 @@ resource "google_compute_instance" "default" {
       apt-get install -y docker.io
       systemctl enable docker
       systemctl start docker
-      docker pull ${var.frontend_image}
-      docker pull ${var.backend_image}
-      docker run -d -p 8000:8000 ${var.frontend_image}
-      docker run -d -p 3000:3000 ${var.backend_image}
+
+      echo "Pulling frontend image..." > /var/log/startup.log
+      docker pull ${var.frontend_image} >> /var/log/startup.log 2>&1
+      echo "Pulling backend image..." >> /var/log/startup.log
+      docker pull ${var.backend_image} >> /var/log/startup.log 2>&1
+
+      echo "Starting frontend container..." >> /var/log/startup.log
+      docker run -d -p 8000:8000 ${var.frontend_image} >> /var/log/startup.log 2>&1
+      echo "Starting backend container..." >> /var/log/startup.log
+      docker run -d -p 3000:3000 ${var.backend_image} >> /var/log/startup.log 2>&1
     EOT
   }
 }
@@ -55,4 +61,9 @@ resource "google_compute_firewall" "default" {
   source_ranges = ["0.0.0.0/0"]  # Allows incoming traffic from any IP address.
 
   target_tags = ["http-server", "https-server"]
+}
+
+# Output the external IP address of the VM
+output "instance_external_ip" {
+  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
