f972f8d3e8d20844d8ac9e622637f20350e6de9f
ngumonelson123 <nelsonmbui88@gmail.com>
2024-11-06 20:28:04 +0300

    update variables.tf file

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "GOOGLE_APPLICATION_CREDENTIALS" {
+  description = "/home/nelson-ngumo/Documents/burnished-ether-439413-s1-579bee90267c.json"
+  type        = string
+  default     = ""
+}
