d42564cd43964e7a23cece35289312dfce63e265
Denislive <dennygachemi6@gmail.com>
2024-12-20 10:36:14 +0300

    updated settings.py

--- backend/EquipRentHub/settings.py
+++ backend/EquipRentHub/settings.py
@@ -126,14 +126,12 @@ CSRF_TRUSTED_ORIGINS = [
     'https://usenlease-ba2103147f4b.herokuapp.com',
     'https://usenlease.com',
     'https://usenlease-2f8583d212bc.herokuapp.com',
-    "http:localhost:3000"
 
 ]
 CORS_ALLOWED_ORIGINS = [
     'https://usenlease-ba2103147f4b.herokuapp.com',
     'https://usenlease.com',
     'https://usenlease-2f8583d212bc.herokuapp.com',
-    "http:localhost:3000"
 
 
 ]
