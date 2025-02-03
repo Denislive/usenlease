f8470c3aee15f741b0c26d9bcc1ef9d2e55d6058
Denislive <dennygachemi6@gmail.com>
2025-02-04 00:41:00 +0300

    added closing the terminate rental modal

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -617,7 +617,7 @@
 
               <!-- Equipment Image -->
               <div class="p-4 bg-gray-50 flex justify-center">
-                <img :src="chats.find((chat) => chat.id === activeChat)?.equipment.images[0] || 'default-placeholder.jpg'"
+                <img :src="chats.find((chat) => chat.id === activeChat)?.equipment_image || 'default-placeholder.jpg'"
                   alt="Equipment" class="w-full max-w-sm h-auto object-cover rounded-lg shadow-md" />
               </div>
 
@@ -1086,7 +1086,10 @@ export default {
       showModal.value = false;
       selectedOrder.value = null;
       showDeleteConfirm.value = false;
+      showTerminateConfirm.value = false;
     };
+
+  
 
     const confirmDelete = (order) => {
       orderToDelete.value = order;
