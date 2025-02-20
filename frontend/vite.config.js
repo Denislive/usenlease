68a71ad15aac45da29e221b5e9b9fd6907fd8994
ngumonelson123 <nelsonmbui88@gmail.com>
2025-02-20 03:36:25 +0300

    update start.sh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   },
   define: {
     // Hardcoding the API base URL
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://www.usenlease.com'),
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('https://usenlease.com'),
     'import.meta.env.VITE_ENCRYPTION_KEY': JSON.stringify('mr1HukORlqQM1wvE/uEooNbF6cSL3WDT1aeUdRHAleY/ERuSLQo2FXu7z6TLKWIr'),
   },
 });
