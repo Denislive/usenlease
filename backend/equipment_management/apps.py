b35c9f8bf4194f3dc5e626f7284696d70a998366
ngumonelson123 <nelsonmbui88@gmail.com>
2025-03-04 16:51:45 +0300

    setup_periodic_task on app_py

--- backend/equipment_management/apps.py
+++ backend/equipment_management/apps.py
@@ -1,10 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+def setup_periodic_tasks(sender, **kwargs):
+    from .tasks import setup_periodic_task, setup_periodic_task_reduce_equipment
+    setup_periodic_task()
+    setup_periodic_task_reduce_equipment()
 
 class EquipmentManagementConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "equipment_management"
 
     def ready(self):
-        from .tasks import setup_periodic_task, setup_periodic_task_reduce_equipment
-        setup_periodic_task()
-        setup_periodic_task_reduce_equipment()
+        post_migrate.connect(setup_periodic_tasks, sender=self)
